054ce0163f7464f0e435c5256f0745290f7a9ac7
Denis Nuțiu <nutiudenis@hotmail.com>
2016-04-15 23:30:41 +0300

    Creating encrypt and decrypt functions

--- lab8/1.c
+++ lab8/1.c
@@ -1,32 +1,63 @@
 
 #include <stdio.h>
+#include <string.h>
 #include "constants.h"
 #include "cipher.h"
 
-int main () {
-		/*
-		 * Speck 64/96
-		 */
-		u8 inputKey[] = {0x00, 0x01, 0x02, 0x03, 0x08, 0x09, 0x0a, 0x0b, 0x10, 0x11, 0x12, 0x13};
-		u8 keys[SPECK_BLOCK_SIZE/16*SPECK_ROUNDS];
-		// plain text: 74614620 736e6165
-		u8 plainText[] = {0x65, 0x61, 0x6e, 0x73, 0x20, 0x46, 0x61, 0x74};
+void encrypt_file(char * filename, u8 * password) {
+  FILE * in = fopen(filename, "rb");
+  FILE * out = fopen("encrypted.speck", "wb");
 
-		encryptKeySchedule(inputKey, keys);
+  if ( !in ) {
+    perror("Error opening file.");
+  }
+}
 
-		printf("PlainText: %x, %x, %x, %x, %x, %x, %x, %x\n",
-			plainText[0], plainText[1], plainText[2], plainText[3],
-			plainText[4], plainText[5], plainText[6], plainText[7]);
+void decrypt_file(char * filename, u8 * password) {
+  FILE * in = fopen(filename, "rb");
+  FILE * out = fopen("decrypted.speck", "wb");
 
-		encrypt(plainText, keys);
-		// cipher text: 9f7952ec 4175946c
-		printf("After encryption: %x, %x, %x, %x, %x, %x, %x, %x\n",
-			plainText[0], plainText[1], plainText[2], plainText[3],
-			plainText[4], plainText[5], plainText[6], plainText[7]);
 
-		decrypt(plainText, keys);
-		printf("After decryption: %x, %x, %x, %x, %x, %x, %x, %x\n",
-			plainText[0], plainText[1], plainText[2], plainText[3],
-			plainText[4], plainText[5], plainText[6], plainText[7]);
+  if ( !in ) {
+    perror("Error opening file.");
+  }
+}
+
+int main (int argc, char *argv[]) {
+
+  if ( argc != 4) {
+    printf("Invalid usage! Correct usage %s filname password [1/0 which represent"\
+    "encrypt or decrypt]\n", argv[0]);
+    return 1;
+  }
+
+  char * password = argv[2];
+  u8 inputKey[strlen(password) - 1];
+  int i = 0;
+  for (; password[i]; i++) {
+    inputKey[i] = password[i];
+  }
+  u8 keys[SPECK_BLOCK_SIZE/16*SPECK_ROUNDS];
+  encryptKeySchedule(inputKey, keys);
+
+  u8 plainText[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+
+	//
+	// 	printf("PlainText: %c, %c, %c, %c, %c, %c, %c, %c\n",
+	// 		plainText[0], plainText[1], plainText[2], plainText[3],
+	// 		plainText[4], plainText[5], plainText[6], plainText[7]);
+	//
+  // encrypt(plainText, keys);
+	//
+	// 	printf("After encryption: %c, %c, %c, %c, %c, %c, %c, %c\n",
+	// 		plainText[0], plainText[1], plainText[2], plainText[3],
+	// 		plainText[4], plainText[5], plainText[6], plainText[7]);
+	//
+  // decrypt(plainText, keys);
+	// 	printf("After decryption: %c, %c, %c, %c, %c, %c, %c, %c\n",
+	// 		plainText[0], plainText[1], plainText[2], plainText[3],
+	// 		plainText[4], plainText[5], plainText[6], plainText[7]);
+
 	return 0;
 }
